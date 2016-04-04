fc645cde0ab8f17b56a0c035bdc402b1017f3ff7
Dommert <DommertEnterprises@Gmail.com>
2016-04-03 17:44:27 -0700

    new nav bar

--- app/assets/js/app.js
+++ app/assets/js/app.js
@@ -1,4 +1,4 @@
-var webApp = angular.module('webApp', ['ui.bootstrap', 'ui.router']);
+var webApp = angular.module('webApp', ['ui.bootstrap', 'ui.router', 'ngAnimate']);
 
 webApp.controller('mainCtrl',
 	function($scope){
@@ -8,6 +8,30 @@ webApp.controller('mainCtrl',
 webApp.controller('buttonCtrl', 
 	function($scope){
 		$scope.singleModel = 1;
+});
+
+
+webApp.controller('MainCtrl', function($scope) {
+
+    $scope.name = 'World';
+
+});
+
+
+webApp.controller('DropdownCtrl', function($scope) {
+
+ 
+
+    $scope.items = [
+
+        "The first choice!",
+
+        "And another choice for you.",
+
+        "but wait! A third!"
+
+    ];
+
 });
 
 
